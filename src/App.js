40b9b8f81ca917feee928112471c72b9be36e09b
PoojaBala1993 <poojabalanehru2023@u.northwestern.edu>
2023-08-19 10:49:19 -0400

    Getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
+import Employee from './components/Employee'
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
+  const showEmployee = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-
-      </header>
-    </div>
+      {showEmployee ? (
+        <>
+          <input type="text" onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }}
+          />
+          <Employee name="Pooja" role="intern" />
+          <Employee name="John" role={role} />
+          <Employee name="Jenny" />
+        </>
+      ) : (
+        <p>Cannot show the employees</p>
+      )
+      }
+    </div >
   );
 }
 
---
+++ src/components/Employee.js
@@ -0,0 +1,10 @@
+function Employee(props) {
+    return (
+        <>
+            <h3>Here is {props.name}</h3>
+            <p>{props.role ? props.role : 'No role'}</p>
+        </>
+    )
+}
+
+export default Employee;
